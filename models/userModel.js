9a7c71c4d00021d10905594c0be9176251bd01a6
Ashrafpoless <150967649+Ashrafpoless@users.noreply.github.com>
2025-02-17 23:15:40 +0100

    user

---
+++ models/userModel.js
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    cart: { type:Object, default:{},ref: "Food" },
+}, {minimize: false});
+
+const UserModel = mongoose.model.user || mongoose.model("user", userSchema);
+
+export default UserModel;
---
+++ routes/userRoute.js
@@ -0,0 +1,10 @@
+import express from "express"
+import userController from "../controllers/userController.js"
+import multer from "multer"
+
+const userRouter = express.Router()
+
+userRouter.post('/register',userController.registerUser)
+userRouter.post('/login',userController.loginUser)
+
+export default userRouter;
