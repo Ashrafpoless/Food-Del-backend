202d644644a3bb4cbef04f1a73c199d6ff1b17e3
Ashrafpoless <150967649+Ashrafpoless@users.noreply.github.com>
2025-03-11 19:42:23 +0100

    upload

--- routes/foodRoute.js
+++ routes/foodRoute.js
@@ -1,17 +1,28 @@
 import express from 'express';
 import foodController from '../controllers/foodController.js';
 import multer from 'multer';
+import path from "path";
 
 const foodRouter = express.Router();
 
 // Image Storage Engine
 
-const storage = multer.diskStorage({
+/*const storage = multer.diskStorage({
     destination: 'uploads',
     filename: (req, file, cb) => {
         return cb(null, `${Date.now()}${file.originalname}`);
     }
-});
+});*/
+
+
+const storage = multer.diskStorage({
+    destination:  (req, file, cb)=> {
+      cb(null, "uploads"); // ✅ Vercel allows writing here
+    },
+    filename: (req, file, cb)=> {
+      cb(null, Date.now() + path.extname(file.originalname));
+    },
+  });
 
 const upload = multer({ storage: storage });
 
