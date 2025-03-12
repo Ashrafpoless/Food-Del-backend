fc1369d48094ae09e7e8ecec25823701f4c38763
Ashrafpoless <150967649+Ashrafpoless@users.noreply.github.com>
2025-03-12 14:38:51 +0100

    Update db.js

--- config/db.js
+++ config/db.js
@@ -3,10 +3,7 @@ import mongoose from 'mongoose';
 // Connect to MongoDB
 export const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.MONGODB_URI, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        });
+        await mongoose.connect(process.env.MONGODB_URI);
         console.log('MongoDB Connected...');
     } catch (error) {
         console.error('Error connecting to MongoDB:', error.message);
